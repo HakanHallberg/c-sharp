a0df178093f14c07db727823459f6c54b433ec82
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2017-09-11 14:11:38 +0200

    Made and tested Simple and While. Green light!

---
+++ MultiplierTestLib/MultiClassTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiplierTestLib
+{
+    [TestClass]
+    public class MultiClassTest
+    {
+        [TestMethod]
+        public void While()
+        {
+            //Arrange
+            MultiplierLib.MultiClass myTest = new MultiplierLib.MultiClass();
+            //Act
+            var actual = myTest.Simple(20, 20);
+            
+            //Assert
+            Assert.AreEqual(400, actual);
+        }
+
+    }
+}
