f2dc4a640203d66727ea4485a1afa490ae3073c2
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2017-09-13 12:45:47 +0200

    Green light on DaysBetween... och Add1000Days...

---
+++ MultiplierLib/DateHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+    public class DateHandler
+    {
+        public int DaysBetweenDates(DateTime firstDate, DateTime secondDate)
+        {
+            int numberOfDays = Convert.ToInt32((secondDate - firstDate).TotalDays);
+            return numberOfDays;
+        }
+
+        public DateTime Add1000Days(DateTime theDate)
+        {
+            DateTime myDate = theDate.AddDays(1000);
+            return myDate;
+        }
+    }
+}
