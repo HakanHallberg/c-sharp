14c949183d3cabb26716f294e0db04ae626f266c
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2017-09-18 10:59:54 +0200

    Created and got green light on "Varukorg"

---
+++ VarukorgTestLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VarukorgTestLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VarukorgTestLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f055388e-325a-454a-b080-1a5d61b3f24b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VarukorgTestLib/VarukorgTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VarukorgLib;
+
+namespace VarukorgTestLib
+{
+    [TestClass]
+    public class VarukorgTest
+    {
+        [TestMethod]
+        public void Orderbekräftelse()
+        {
+            Kund testKund = new Kund("Test Testsson");
+            Produkt testLaptop = new Produkt("Laptop test");
+            testLaptop.Pris = 4000;
+            Produkt testSkärm = new Produkt("Skärm test");
+            testSkärm.Pris = 1500;
+            Varukorg sut = new Varukorg();
+            sut.Kund = testKund;
+            sut.LäggTillProdukt(testLaptop);
+            sut.LäggTillProdukt(testSkärm);
+            var expected = "Test Testsson har beställt varor för 5500 kr";
+
+            sut.Beställ();
+            Orderbekräftelse result = sut.Orderbekräftelse;
+            string actual = result.Meddelande;
+
+            Assert.AreEqual(expected, actual);
+         }
+    }
+}
