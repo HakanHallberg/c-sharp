af2b15ce59794dbf0bf68812138dfb46e2ba76c8
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2017-09-20 13:54:12 +0200

    Created ExamLib and ExamTestLib and got a green light.

---
+++ ExamLib/ExamLib.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamLib
+{
+    public class Exam
+    {
+        private string coursename;
+        //private object namn;
+        private Dictionary<Student, string> StudentGradeDictionary =
+            new Dictionary<Student, string>();
+
+        public Exam(string coursename)
+        {
+            this.coursename = coursename;
+        }
+
+        public void Assign(Student student)
+        {
+            StudentGradeDictionary.Add(student, "");
+        }
+
+        public void Grade(Student student, string grade)
+        {
+            if (!StudentGradeDictionary.ContainsKey(student))
+            throw new UnassignedStudentException();
+            StudentGradeDictionary[student] = grade;
+            
+
+            
+        }
+
+        public Dictionary<string, int> GenerateStatistics()
+        {
+            var gradeCountDicitionary = new Dictionary<string, int>();
+            gradeCountDicitionary.Add("IG", 0);
+            gradeCountDicitionary.Add("G", 0);
+            gradeCountDicitionary.Add("VG", 0);
+            foreach (var StudentGradeEntry in StudentGradeDictionary)
+            {
+                gradeCountDicitionary[StudentGradeEntry.Value]++;
+            }
+            return gradeCountDicitionary;
+        }
+    }
+
+}
---
+++ ExamTestLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExamTestLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExamTestLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86491335-30e0-4801-9d60-d2903709c664")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
