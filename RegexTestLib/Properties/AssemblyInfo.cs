c4755e022ccdc28eccea8d75bbc19c383fd681dd
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2017-10-11 09:45:15 +0200

    Regex exercise with accounting started

---
+++ RegexTestLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RegexTestLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegexTestLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6d95e68-e930-4334-af3a-6389cbe8d750")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RegexTestLib/RegexTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Diagnostics;
+
+namespace RegexTestLib
+{
+    [TestClass]
+    public class RegexTest
+    {
+        [TestMethod]
+        public void CanRead()
+        {
+            var streamReader = File.OpenText(@"SIEI.txt");
+            while (true)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null)
+                    break;
+                Debug.WriteLine(line);
+            }
+        }
+    }
+}
